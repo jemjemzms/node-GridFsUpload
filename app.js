8e8073a1dd7724fba7862c3fa1e9ce8ba0c68628
Jed Diaz <jemjemzms@gmail.com>
2019-02-01 01:04:53 +1300

    File Display
    Sending the files to the ejs template and displaying it to the template

--- app.js
+++ app.js
@@ -56,7 +56,24 @@ const upload = multer({ storage });
 // @route Get /
 // @desc Loads form
 app.get("/", (req, res) => {
-  res.render("index");
+  gfs.files.find().toArray((err, files) => {
+    // Check if files
+    if (!files || files.length === 0) {
+      res.render("index", { files: false });
+    } else {
+      files.map(file => {
+        if (
+          file.contentType === "image/jpeg" ||
+          file.contentType === "image/png"
+        ) {
+          file.isImage = true;
+        } else {
+          file.isImage = false;
+        }
+      });
+      res.render("index", { files: files });
+    }
+  });
 });
 
 // @route POST /upload
